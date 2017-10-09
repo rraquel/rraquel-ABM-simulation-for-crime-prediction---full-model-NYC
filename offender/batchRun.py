d6ede3d1de52fe48a09747ef0a663ebe2d0511ac
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2017-10-09 16:55:09 +0200

    50 steps

--- offender/batchRun.py
+++ offender/batchRun.py
@@ -1,7 +1,5 @@
 #!/bin/batchRun
 
-for i in test1.ini test2.ini test3.ini default.ini; do
-    offender/run.py $i &
-    sleep 1
-done
-i in
+#for i in test1.ini test2.ini test3.ini default.ini; do
+for i in config/test1.ini, config/test2.ini:
+    offender/run.py
