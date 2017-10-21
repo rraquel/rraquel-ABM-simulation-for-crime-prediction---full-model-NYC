075e4c609b39bb65b7e0861e9c7236c2be29b30b
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2017-10-21 10:51:56 +0200

    test

--- offender/multirun.sh
+++ offender/multirun.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for i in config/perftest1/*.ini; do
+#for i in config/perftest1/*.ini; do
+for i in config/perftest1/*; do 
     offender/runOffender.py `echo "$i"|sed -e 's:config/::'` &
     sleep 1
 done
