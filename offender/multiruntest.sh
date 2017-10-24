689cb88f941141746d58fbd791e0f5d91b3b31f2
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2017-10-24 14:57:32 +0200

    test

--- offender/multiruntest.sh
+++ offender/multiruntest.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #list=[]
+#to run: in pythonprototype folder: python3 offender/multirun.sh
 
 #for i in config/perftest1/*.ini; do
-for i in config/pretest/test1).ini config/pretest/test2).ini ; do 
+for i in config/pretest/test1.ini config/pretest/test2.ini ; do 
     offender/run.py `echo "$i"|sed -e 's:config/::'` &
     sleep 1
 done
