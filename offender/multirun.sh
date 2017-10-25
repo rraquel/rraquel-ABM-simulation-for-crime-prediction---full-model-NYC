db7adb0fec0f5cd62d17c156fe650b940ae03080
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2017-10-25 15:49:32 +0200

    test

--- offender/multirun.sh
+++ offender/multirun.sh
@@ -10,21 +10,21 @@ for i in config/pretest/test1.ini config/pretest/test2.ini config/pretest/test3.
 done
 
 #offender/run.py blubb.ini | at now + 7 hours
-"""
-while true; do
-  NUMPROCS=`ps -ef| grep run.py | wc -l`
-  if [$NUMPROCS -le 1]; then
-    # starte zweite Ladung
-    for i in config/pretest/test10.ini config/pretest/test11.ini config/pretest/test12.ini config/pretest/test13.ini config/pretest/test14.ini config/pretest/test15.ini config/pretest/test16.ini config/pretest/test17.ini config/pretest/test18.ini ; do 
-    offender/run.py `echo "$i"|sed -e 's:config/::'` &
-    sleep 1
-done
-    break
-  fi
-  sleep 10
-done
 
- """"
+#while true; do
+#  NUMPROCS=`ps -ef| grep run.py | wc -l`
+#  if [$NUMPROCS -le 1]; then
+#    # starte zweite Ladung
+#    for i in config/pretest/test10.ini config/pretest/test11.ini config/pretest/test12.ini config/pretest/test13.ini config/pretest/test14.ini config/pretest/test15.ini config/pretest/test16.ini config/pretest/test17.ini config/pretest/test18.ini ; do 
+#    offender/run.py `echo "$i"|sed -e 's:config/::'` &
+#    sleep 1
+#done
+    break
+#  fi
+#  sleep 10
+#done
+
+
 
  # ./multirun.sh |at now + 7 hours
   
