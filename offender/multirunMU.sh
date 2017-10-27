a2c40efe148cc720711d60e6f9b25d6e554b3e90
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2017-10-27 16:12:40 +0200

    test

--- offender/multirunMU.sh
+++ offender/multirunMU.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#for i in config/pretestMU/* ; do 
-#    offender/run.py `echo "$i"|sed -e 's:config/::'` &
-#    sleep 1
-#done
-
-
-
-for i in config/pretestMU/test1.ini config/pretestMU/test2.ini config/pretestMU/test3.ini config/pretestMU/test4.ini config/pretestMU/test5.ini config/pretestMU/test6.ini config/pretestMU/test7.ini config/pretestMU/test8.ini config/pretestMU/test9.ini config/pretestMU/test10.ini ; do 
+for i in config/pretestMU/* ; do 
     offender/run.py `echo "$i"|sed -e 's:config/::'` &
     sleep 1
 done
+
+
+
+#for i in config/pretestMU/test1.ini config/pretestMU/test2.ini config/pretestMU/test3.ini config/pretestMU/test4.ini config/pretestMU/test5.ini config/pretestMU/test6.ini config/pretestMU/test7.ini config/pretestMU/test8.ini config/pretestMU/test9.ini config/pretestMU/test10.ini ; do 
+#    offender/run.py `echo "$i"|sed -e 's:config/::'` &
+#    sleep 1
+#done
