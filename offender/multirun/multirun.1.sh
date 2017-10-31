aedebaa735c51ef2df33203d080f0db2e9251ae8
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2017-10-31 12:16:04 +0100

    test

--- offender/multirun/multirun.1.sh
+++ offender/multirun/multirun.1.sh
@@ -15,15 +15,17 @@
 
 
 ########### first batch ###############
-
-
-
-############second batch ###############
-
-for i in config/pretest114Pop/test10.ini config/pretest114Pop/test11.ini config/pretest114Pop/test12.ini config/pretest114Pop/test13.ini config/pretest114Pop/test14.ini config/pretest114Pop/test15.ini config/pretest114Pop/test16.ini config/pretest114Pop/test17.ini config/pretest114Pop/test18.ini ; do 
+for i in config/pretest/test1.ini config/pretest/test2.ini config/pretest/test3.ini config/pretest/test4.ini config/pretest/test5.ini config/pretest/test6.ini config/pretest/test7.ini config/pretest/test8.ini config/pretest/test9.ini ; do 
     offender/run.py `echo "$i"|sed -e 's:config/::'` &
     sleep 1
 done
+
+############second batch ###############
+
+#for i in config/pretest2/test10.ini config/pretest2/test11.ini config/pretest2/test12.ini config/pretest2/test13.ini config/pretest2/test14.ini config/pretest2/test15.ini config/pretest2/test16.ini config/pretest2/test17.ini config/pretest2/test18.ini ; do 
+#    offender/run.py `echo "$i"|sed -e 's:config/::'` &
+#    sleep 1
+#done
 
 #for i in config/pretest/test10.ini config/pretest/test11.ini config/pretest/test12.ini config/pretest/test13.ini config/pretest/test14.ini config/pretest/test15.ini config/pretest/test16.ini config/pretest/test17.ini config/pretest/test18.ini ; do 
 #    offender/run.py `echo "$i"|sed -e 's:config/::'` &
@@ -36,7 +38,7 @@ done
 #  NUMPROCS=`ps -ef| grep run.py | wc -l`
 #  if [$NUMPROCS -le 1]; then
 #    # starte zweite Ladung
-#    for i in config/testSequential/test2.ini ; do 
+#    for i in config/testSequential/* ; do 
 #    offender/run.py `echo "$i"|sed -e 's:config/::'` &
 #    sleep 1
 #done
---
+++ offender/viewresults/aamasplots.py
@@ -0,0 +1,16 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+"""plot unique crime hit rate vs. num of agent per strategy"""
+#all cirmes
+
+#burlgary
+
+#robbery
+
+#larceny
+
+#larcenyM
+
+#assault
