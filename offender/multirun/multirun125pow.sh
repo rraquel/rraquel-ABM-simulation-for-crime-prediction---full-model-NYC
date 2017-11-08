ed8536e4811d6fe8ea6b05b43aad9438d78b7784
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2017-11-08 10:32:42 +0100

    test

--- offender/multirun/multirun125pow.sh
+++ offender/multirun/multirun125pow.sh
@@ -11,28 +11,4 @@ done
   sleep 600
 done
 
-while true; do
-  NUMPROCS=`ps -ef| grep run.py | wc -l`
-  if [ $NUMPROCS -le 3 ]; then
-    # starte zweite Ladung
-    for i in config/test/125/test125PPVC.ini config/test/125/test125PRV.ini; do 
-    offender/run.py `echo "$i"|sed -e 's:config/::'` &
-    sleep 10
-done
-    break
-  fi
-  sleep 600
-done
 
-while true; do
-  NUMPROCS=`ps -ef| grep run.py | wc -l`
-  if [ $NUMPROCS -le 3 ]; then
-    # starte zweite Ladung
-    for i in config/test/125/test125PRVC; do 
-    offender/run.py `echo "$i"|sed -e 's:config/::'` &
-    sleep 10
-done
-    break
-  fi
-  sleep 600
-done
