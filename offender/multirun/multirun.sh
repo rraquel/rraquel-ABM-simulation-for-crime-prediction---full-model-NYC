f5e828ebd7c1ba07f2563e7bbd8d1163043e8ccf
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2018-11-16 11:50:58 +0100

    p_PV

--- offender/multirun/multirun.sh
+++ offender/multirun/multirun.sh
@@ -11,7 +11,7 @@
 #done
 
 
-for i in config/test1000_new/2/1000_p_R*.ini ; do 
+for i in config/test1000_new/2/1000_p_PV.ini ; do 
     python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
     sleep 100
 done
