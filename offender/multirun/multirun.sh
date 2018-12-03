d71ab1a30c7c55b47e71781156c2d26dfe56f62a
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2018-12-03 10:08:53 +0100

    run 3

--- offender/multirun/multirun.sh
+++ offender/multirun/multirun.sh
@@ -12,26 +12,21 @@
 #ssd
 
 
-runs 10
-for i in config/test1000_new/all/1000_ct12*.ini ; do 
+
+#runs 1
+for i in config/test1000_new/all/1000_ct6*.ini ; do 
     python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
     sleep 100
 done
 
-#runs 1
-#for i in config/test1000_new/all/1000_ct6*.ini ; do 
-#    python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
-#    sleep 100
-#done
-
-#for i in config/test1000_new/all/1000_ct2*.ini ; do 
-#    python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
-#    sleep 100
-#done
-#for i in config/test1000_new/all/1000_ct1_*.ini ; do 
-#    python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
-#    sleep 100
-#done
+for i in config/test1000_new/all/1000_ct2*.ini ; do 
+    python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
+    sleep 100
+done
+for i in config/test1000_new/all/1000_ct1_*.ini ; do 
+    python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
+    sleep 100
+done
 
 
 
