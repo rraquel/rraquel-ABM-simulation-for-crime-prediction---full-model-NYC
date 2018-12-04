34910256d437e2c952fd2b976ee8396365fe9c00
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2018-12-04 13:35:48 +0100

    run 4

--- offender/multirun/multirun.sh
+++ offender/multirun/multirun.sh
@@ -13,13 +13,7 @@
 
 
 #runs 4
-for i in config/test1000_new/10/1000_t.ini ; do 
-    python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
-    sleep 10
-done
-
-#runs 2
-for i in config/test1000_new/10/1000_p*.ini ; do 
+for i in config/test1000_new/10/1000_t*.ini ; do 
     python3 offender/run.py `echo "$i"|sed -e 's:config/::'` &
     sleep 10
 done
