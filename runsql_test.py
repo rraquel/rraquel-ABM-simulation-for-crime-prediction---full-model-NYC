a8be1bcd99bc1ffc63970ff1197cca6b2fcf521e
Rosés Brüngger  Raquel <rraquel@ethz.ch>
2018-12-07 10:15:29 +0100

    delete roads from deleted run_id's in amb_res.res_la_runprototype

--- runsql_test.py
+++ runsql_test.py
@@ -8,12 +8,19 @@ conn= psycopg2.connect("dbname='shared' user='rraquel' host='127.0.0.1' password
 mycurs=conn.cursor()
 
 print("start query")
-mycurs.execute("""Insert into abm_res.res_la_roadsprototype2 ("run_id","step","agent","road_id", i, trip)
-select run_id, step, agent, road_id, i, trip from abm_res.res_la_roadsprototype
-where run_id in (select run_id from abm_res.res_la_runprototype)""")
+#mycurs.execute("""Insert into abm_res.res_la_roadsprototype2 ("run_id","step","agent","road_id", i, trip)
+#select run_id, step, agent, road_id, i, trip from abm_res.res_la_roadsprototype
+#where run_id in (select run_id from abm_res.res_la_runprototype)""")
+
+mycurs.execute("""DELETE FROM abm_res.res_la_roadsprototype
+	WHERE run_id NOT IN (SELECT run_id FROM abm_res.res_la_runprototype)""")
 
 conn.commit()
-mycurs.execute("""select * from abm_res.res_la_roadsprototype2 limit 10""")
+mycurs.execute("""DELETE FROM abm_res.res_la_agentprototype
+	WHERE run_id NOT IN (SELECT run_id FROM abm_res.res_la_runprototype)""")
+conn.commit()  
+#mycurs.execute("""select * from abm_res.res_la_roadsprototype2 limit 10""")
 result=mycurs.fetchall()
 print(result[0])
+conn.close()
 
